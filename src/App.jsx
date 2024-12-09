19b78a4d9cce45b31b8108759633cc6da9d657fe
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-09 12:06:25 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-
-  return <></>;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="relative w-screen h-screen overflow-auto snap-y snap-mandatory">
+      <div className="w-full h-screen snap-always snap-start ">Home</div>
+      <div className="w-full h-screen snap-always snap-start"></div>
+    </main>
+  );
+};
+
+export default Home;
