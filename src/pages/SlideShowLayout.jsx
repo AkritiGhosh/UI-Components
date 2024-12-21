b488601678c54c54a2fd1431c3ea9a1a9f4b0eb3
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-21 11:18:58 +0530

    Header added

--- src/pages/SlideShowLayout.jsx
+++ src/pages/SlideShowLayout.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ArrowIcon from "../components/carousel/arrowIcon";
+import Header from "../components/carousel/Header";
 
 const SlideShowLayout = ({ children }) => {
   return (
-    <div className="w-full h-screen ">
-      <span className="absolute cursor-pointer w-16 h-full top-0 left-0 flex items-center justify-start px-2 hover:bg-gradient-to-r from-indigo-200 to-white group  transition-all duration-300">
+    <div className="w-full h-screen relative">
+      <Header />
+      <span className="absolute cursor-pointer w-16 h-full top-0 left-0 flex items-center justify-start px-2 hover:bg-gradient-to-r from-slate-200 to-white group  transition-all duration-300">
         <ArrowIcon />
       </span>
-      <span className="absolute cursor-pointer w-16 h-full top-0 right-0 flex items-center justify-end px-2 hover:bg-gradient-to-r from-white to-indigo-200 group  transition-all duration-300">
+      <span className="absolute cursor-pointer w-16 h-full top-0 right-0 flex items-center justify-end px-2 hover:bg-gradient-to-r from-white to-slate-200 group  transition-all duration-300">
         <ArrowIcon left={false} />
       </span>
       <div className="w-[calc(100%-128px)] mx-auto h-full">{children}</div>
