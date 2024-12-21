06a52c170a2c892d3bb1a37abe2ebe3126fe3cd0
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-21 10:04:37 +0530

    slideshow UI arrows

--- src/pages/SlideShowLayout.jsx
+++ src/pages/SlideShowLayout.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import ArrowIcon from "../components/carousel/arrowIcon";
 
-const SlideShowLayout = () => {
+const SlideShowLayout = ({ children }) => {
   return (
-    <div>SlideShowLayout</div>
-  )
-}
+    <div className="w-full h-screen ">
+      <span className="absolute cursor-pointer w-16 h-full top-0 left-0 flex items-center justify-start px-2 hover:bg-gradient-to-r from-indigo-200 to-white group  transition-all duration-300">
+        <ArrowIcon />
+      </span>
+      <span className="absolute cursor-pointer w-16 h-full top-0 right-0 flex items-center justify-end px-2 hover:bg-gradient-to-r from-white to-indigo-200 group  transition-all duration-300">
+        <ArrowIcon left={false} />
+      </span>
+      <div className="w-[calc(100%-128px)] mx-auto h-full">{children}</div>
+    </div>
+  );
+};
 
-export default SlideShowLayout
+export default SlideShowLayout;
