12d542d82eb373ec3b6e98a15cf924534dd5d7c4
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-21 11:17:17 +0530

    Update ArrowIcon.jsx

--- src/components/carousel/ArrowIcon.jsx
+++ src/components/carousel/ArrowIcon.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 const ArrowIcon = ({ left = true }) => {
   const position = left ? "left-0" : "right-0";
-  const rotateTop = left ? "group-hover:rotate-45" : "group-hover:-rotate-45";
+  const rotateTop = left
+    ? "group-hover:rotate-[30deg]"
+    : "group-hover:-rotate-[30deg]";
   const rotateBottom = !left
-    ? "group-hover:rotate-45"
-    : "group-hover:-rotate-45";
+    ? "group-hover:rotate-[30deg]"
+    : "group-hover:-rotate-[30deg]";
   return (
     <div className={"w-2 h-10 relative group " + position}>
       <div
-        className={`w-2 h-10 bg-indigo-950 rounded-sm rounded-t-lg relative top-0 left-0 rotate-0 origin-bottom  ${rotateTop} transition-all duration-300`}
+        className={`w-1 h-10 bg-slate-700 rounded-sm rounded-t-lg relative top-0 left-0 rotate-0 origin-bottom  ${rotateTop} transition-all duration-300`}
       />
       <div
-        className={`w-2 h-10 bg-indigo-950 rounded-sm rounded-b-lg relative -top-1 left-0 rotate-0 origin-top ${rotateBottom}  transition-all duration-300`}
+        className={`w-1 h-10 bg-slate-700 rounded-sm rounded-b-lg relative -top-0.5 left-0 rotate-0 origin-top ${rotateBottom}  transition-all duration-300`}
       />
     </div>
   );
