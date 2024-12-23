4c0383afbcbd098460b0f3729089f2724c7de67c
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-23 10:22:22 +0530

    data file type updated for imports

---
+++ src/lib/data.jsx
@@ -0,0 +1,15 @@
+import FloatingLabelTextfield from "../components/form_elements/FloatingLabelInputField";
+
+ const data = [
+  {
+    heading: "Floating Label for Input",
+    id: "floating_label_textfield",
+    file: <FloatingLabelTextfield />,
+    code: null,
+    article: null,
+    backId: null,
+    nextId: null,
+  },
+];
+
+export default data;
