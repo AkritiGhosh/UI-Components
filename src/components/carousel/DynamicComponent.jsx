fdef1100639880bb2e60851feafbfcad9e21c5ab
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-23 10:21:59 +0530

    Delete DynamicComponent.jsx

--- src/components/carousel/DynamicComponent.jsx
+++
@@ -1,8 +0,0 @@
-import React, { lazy } from "react";
-
-const DynamicComponent = ({ path, ...props }) => {
-  const ImportedComponent = lazy(() => import(path));
-  return <ImportedComponent {...props} />;
-};
-
-export default DynamicComponent;
