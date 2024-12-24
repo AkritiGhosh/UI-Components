75ffbb98527b612fd1d102dad31c086fe5a1d1d1
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-24 10:28:45 +0530

    add hero section to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import HeroSection from "../components/homepage/HeroSection";
 
 const Home = () => {
   return (
-    <main className="relative w-screen h-screen overflow-auto snap-y snap-mandatory bg-app-200">
-      <div className="w-full h-screen snap-always snap-start bg-app-950"></div>
+    <main className="relative w-screen h-screen overflow-x-hidden overflow-y-auto snap-y snap-mandatory bg-app-200">
+      <div className="w-full h-screen snap-always snap-start bg-app-950">
+        <HeroSection/>
+      </div>
       <div className="relative w-full h-screen snap-always snap-start bg-app-950">
         <h1 className=" underline text-7xl text-center pt-5 bg-inherit bg-clip-text">
           Feature list
