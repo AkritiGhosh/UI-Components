5ce0f9eac7ded57f7f60cfeb2ed88132bbdfd155
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-24 17:25:31 +0530

    Create FeatureSection.jsx

---
+++ src/components/homepage/FeatureSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FeatureSection = () => {
+  return (
+    <>
+      <h1 className=" underline text-7xl text-center pt-5 bg-inherit bg-clip-text">
+        Feature list
+      </h1>
+    </>
+  );
+};
+
+export default FeatureSection;
