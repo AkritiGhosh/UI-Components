d5bbe5ec25ba95ae6899a122f74262de311b1e93
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-25 18:01:06 +0530

    dark theme- added

--- src/lib/data.jsx
+++ src/lib/data.jsx
@@ -1,3 +1,4 @@
+import GradientBorder1 from "../components/buttons/colorful_border/GradientBorder1";
 import FloatingLabelTextfield from "../components/form_elements/FloatingLabelInputField";
 import ImageToggleSwitch from "../components/form_elements/ImageSwitch";
 
@@ -18,6 +19,15 @@ const data = [
     code: "https://github.com/AkritiGhosh/UI-Components/blob/main/src/components/form_elements/ImageSwitch.jsx",
     article: null,
     backId: "floating_label_textfield",
+    nextId: "gradient_border1",
+  },
+  {
+    heading: "Gradient Border - Method 1",
+    id: "gradient_border1",
+    file: <GradientBorder1 />,
+    code: "https://github.com/AkritiGhosh/UI-Components/blob/main/src/components/form_elements/ImageSwitch.jsx",
+    article: null,
+    backId: "image_switch",
     nextId: null,
   },
 ];
--- src/pages/SlideShowLayout.jsx
+++ src/pages/SlideShowLayout.jsx
@@ -19,12 +19,12 @@ const SlideShowLayout = () => {
   const handleNext = () => nav("/component/" + pageData?.nextId);
 
   return (
-    <div className="w-full h-screen relative flex flex-col">
+    <div className="w-full h-screen relative flex flex-col bg-white dark:bg-black">
       <Header isDark={isDark} setIsDark={setIsDark} />
       {pageData?.backId && (
         <span
           onClick={handleBack}
-          className="absolute cursor-pointer w-16 h-full top-0 left-0 flex items-center justify-start px-2 hover:bg-gradient-to-r from-app-200 to-white group  transition-all duration-300"
+          className="absolute cursor-pointer w-16 h-full top-0 left-0 flex items-center justify-start px-2 hover:bg-gradient-to-r from-app-200 dark:from-app-950 to-white dark:to-black group  transition-all duration-300"
         >
           <ArrowIcon />
         </span>
@@ -32,12 +32,12 @@ const SlideShowLayout = () => {
       {pageData?.nextId && (
         <span
           onClick={handleNext}
-          className="absolute cursor-pointer w-16 h-full top-0 right-0 flex items-center justify-end px-2 hover:bg-gradient-to-r from-white to-app-200 group  transition-all duration-300"
+          className="absolute cursor-pointer w-16 h-full top-0 right-0 flex items-center justify-end px-2 hover:bg-gradient-to-r from-white dark:from-black to-app-200 dark:to-app-950 group  transition-all duration-300"
         >
           <ArrowIcon left={false} />
         </span>
       )}
-      <div className="w-[calc(100%-128px)] mx-auto h-full flex items-center justify-center">
+      <div className="w-[calc(100%-128px)] mx-auto h-full flex items-center justify-center bg-white dark:bg-black">
         {pageData?.file}
       </div>
     </div>
