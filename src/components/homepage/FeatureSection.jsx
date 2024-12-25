de86c9e843b20e40c9f3c46d62a6b324dcb98bf5
Akriti Ghosh <akritighosh98@gmail.com>
2024-12-25 16:05:12 +0530

    js icons in tag

--- src/components/homepage/FeatureSection.jsx
+++ src/components/homepage/FeatureSection.jsx
@@ -32,8 +32,10 @@ const FeatureSection = () => {
                   {feature?.tags?.length > 0 ? (
                     <div className="w-full flex flex-wrap gap-2">
                       {feature?.tags?.map((tag) => (
-                        <span className="w-auto py-1 px-2 rounded border border-focus-400 text-sm font-normal text-focus-50 flex items-center gap-1">
-                          {tag?.icon ? tag.icon : null}
+                        <span className="w-auto py-1 px-2 rounded border border-gray-700 text-sm font-normal text-focus-50 flex items-center gap-1">
+                          {tag?.icon ? (
+                            <img src={tag.icon} className="size-4 object-cover" />
+                          ) : null}
                           {tag?.data ?? tag}
                         </span>
                       ))}
@@ -48,9 +50,23 @@ const FeatureSection = () => {
                     <Link to={"/component/" + feature?.path}>
                       <button
                         type="button"
-                        className="w-auto py-1 px-2 rounded border border-focus-50 text-sm font-semibold text-focus-50 flex items-center gap-1 hover:border-focus-400"
+                        className="w-auto py-1 px-3 rounded-xl border border-focus-50 text-sm font-semibold text-focus-50 flex items-center gap-2 hover:border-focus-600 group"
                       >
                         Open
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1}
+                          stroke="currentColor"
+                          className="size-4 group-hover:stroke-focus-500 group-hover:stroke-2"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"
+                          />
+                        </svg>
                       </button>
                     </Link>
                   </div>
